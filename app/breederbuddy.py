75f7b645a066f50c8d19ee523debb10cfdf00854
Shawn Haupage <111591051+shawnhaupage@users.noreply.github.com>
2023-02-11 23:17:57 -0600

    Update breederbuddy.py

--- app/breederbuddy.py
+++ app/breederbuddy.py
@@ -1,10 +1,11 @@
 import boto3
 import os
 
-# Connect to the Systems Manager Parameter Store to retrieve the RDS credentials
-ssm = boto3.client('ssm')
-rds_username = ssm.get_parameter(Name='/rds/username')['Parameter']['Value']
-rds_password = ssm.get_parameter(Name='/rds/password')['Parameter']['Value']
+# Connect to the AWS Secrets Manager to retrieve the RDS credentials
+secrets_manager = boto3.client('secretsmanager')
+rds_credentials = secrets_manager.get_secret_value(SecretId='rds_credentials')
+rds_username = rds_credentials['Username']
+rds_password = rds_credentials['Password']
 
 # Connect to the RDS database
 rds = boto3.client(
@@ -52,21 +53,201 @@ def update_mating_plan(plan_id, updated_plan):
         sqlStatements=f'UPDATE mating_plans SET male_dog={updated_plan["male_dog"]}, female_dog={updated_plan["female_dog"]}, planned_date={updated_plan["planned_date"]} WHERE plan_id={plan_id}'
     )
 
-def get_sale_history():
-    # Execute a SQL query to retrieve data about past puppy sales
+# Connect to the AWS S3 service to store images of the puppies
+s3 = boto3.client(
+    's3',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def upload_puppy_image(puppy_id, image_file):
+    # Upload the image file to the S3 bucket
+    s3.upload_file(
+        Filename=image_file,
+        Bucket='breeder-buddy-images',
+        Key=f'puppy_{puppy_id}.jpg'
+    )
+
+def get_puppy_image_url(puppy_id):
+    # Generate a presigned URL for the image file
+    url = s3.generate_presigned_url(
+        ClientMethod='get_object',
+        Params={
+            'Bucket': 'breeder-buddy-images',
+            'Key': f'puppy_{puppy_id}.jpg'
+        }
+    )
+
+    # Return the presigned URL
+    return url
+# Connect to the AWS SNS service to send notifications about new puppies
+sns = boto3.client(
+    'sns',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def notify_new_puppy(puppy_info):
+    # Send a notification about the new puppy to the SNS topic
+    sns.publish(
+        TopicArn='arn:aws:sns:us-west-2:123456789012:new-puppy-notifications',
+        Message=json.dumps({
+            'name': puppy_info['name'],
+            'breed': puppy_info['breed'],
+            'birthdate': puppy_info['birthdate'],
+            'price': puppy_info['price']
+        })
+    )
+
+def notify_mating_plan_update(plan_id, updated_plan):
+    # Send a notification about the updated mating plan to the SNS topic
+    sns.publish(
+        TopicArn='arn:aws:sns:us-west-2:123456789012:mating-plan-update-notifications',
+        Message=json.dumps({
+            'plan_id': plan_id,
+            'male_dog': updated_plan['male_dog'],
+            'female_dog': updated_plan['female_dog'],
+            'planned_date': updated_plan['planned_date']
+        })
+    )
+# Connect to the AWS Rekognition service to perform image recognition on the puppy images
+rekognition = boto3.client(
+    'rekognition',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def recognize_puppy_breed(puppy_id):
+    # Get the URL of the puppy image
+    image_url = get_puppy_image_url(puppy_id)
+
+    # Use the Rekognition service to recognize the breed of the puppy
+    response = rekognition.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': 'breeder-buddy-images',
+                'Name': f'puppy_{puppy_id}.jpg'
+            }
+        }
+    )
+
+    # Return the recognized breed of the puppy
+    return response['Labels'][0]['Name']
+
+# Connect to the AWS S3 service to store images of the puppies
+s3 = boto3.client(
+    's3',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def upload_puppy_image(puppy_id, image_data):
+    # Upload the image data to the S3 bucket
+    s3.upload_fileobj(
+        image_data,
+        'breeder-buddy-images',
+        f'puppy_{puppy_id}.jpg'
+    )
+
+def get_puppy_image_url(puppy_id):
+    # Get the URL of the puppy image
+    return s3.generate_presigned_url(
+        'get_object',
+        Params={
+            'Bucket': 'breeder-buddy-images',
+            'Key': f'puppy_{puppy_id}.jpg'
+        }
+    )
+# Connect to the AWS SNS service to send notifications about new puppies
+sns = boto3.client(
+    'sns',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def send_new_puppy_notification(puppy_info):
+    # Send a notification to all subscribers about the new puppy
+    sns.publish(
+        TopicArn='arn:aws:sns:us-west-2:123456789012:new-puppies',
+        Message=f'A new puppy has been added to the breeding program: {puppy_info["name"]}, {puppy_info["breed"]}, born on {puppy_info["birthdate"]}'
+    )
+
+# Connect to the AWS Comprehend service to perform sentiment analysis on customer reviews
+comprehend = boto3.client(
+    'comprehend',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def analyze_customer_review(review_text):
+    # Use the Comprehend service to analyze the sentiment of the customer review
+    response = comprehend.detect_sentiment(
+        Text=review_text,
+        LanguageCode='en'
+    )
+
+    # Return the sentiment of the customer review
+    return response['Sentiment']
+
+def get_average_customer_rating(puppy_id):
+    # Execute a SQL query to retrieve all customer reviews for a specific puppy
     response = rds.execute_sql(
         secretArn='arn:aws:secretsmanager:us-west-2:123456789012:secret:rds-db-credentials-abcdefg',
         database='mydatabase',
-        sqlStatements='SELECT * FROM sales_history'
+        sqlStatements=f'SELECT * FROM customer_reviews WHERE puppy_id={puppy_id}'
     )
 
-    # Return the results of the query
-    return response['records']
+    # Analyze the sentiment of each customer review
+    sentiments = [analyze_customer_review(review['text']) for review in response['records']]
 
-def add_sale(sale_info):
-    # Execute a SQL query to insert data about a new puppy sale into the database
-    rds.execute_sql(
+    # Calculate the average sentiment of the customer reviews
+    avg_sentiment = sum(sentiments) / len(sentiments)
+
+    # Return the average sentiment as a rating on a scale of 1 to 5
+    return round(avg_sentiment * 2.5 + 2.5)
+
+# Connect to the AWS Rekognition service to perform image recognition on puppy photos
+rekognition = boto3.client(
+    'rekognition',
+    region_name='us-west-2',
+    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY']
+)
+
+def recognize_puppy_breed(puppy_photo):
+    # Use the Rekognition service to recognize the breed of the puppy in the photo
+    response = rekognition.detect_labels(
+        Image={'Bytes': puppy_photo},
+        MaxLabels=1
+    )
+
+    # Return the recognized breed of the puppy
+    return response['Labels'][0]['Name']
+
+def get_puppy_photos(puppy_id):
+    # Execute a SQL query to retrieve all photos for a specific puppy
+    response = rds.execute_sql(
         secretArn='arn:aws:secretsmanager:us-west-2:123456789012:secret:rds-db-credentials-abcdefg',
         database='mydatabase',
-        sqlStatements=f'INSERT INTO sales_history (puppy_id, buyer_name, sale_date, sale_price) VALUES ({sale_info["puppy_id"]}, {sale_info["buyer_name"]}, {sale_info["sale_date"]}, {sale_info["sale_price"]})'
+        sqlStatements=f'SELECT * FROM puppy_photos WHERE puppy_id={puppy_id}'
     )
+
+    # Return the binary data for each photo
+    return [photo['data'] for photo in response['records']]
+
+def classify_puppy_photos(puppy_id):
+    # Get all photos for the puppy
+    puppy_photos = get_puppy_photos(puppy_id)
+
+    # Recognize the breed of the puppy in each photo
+    breeds = [recognize_puppy_breed(photo) for photo in puppy_photos]
+
+    # Return a list of the recognized breeds
+    return breeds
+
